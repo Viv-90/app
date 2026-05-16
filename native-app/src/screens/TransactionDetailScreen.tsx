86c84ce6b8013850eef84d87a19cf9371dc71068
vida <victorademiju.t@gmail.com>
2026-05-16 17:10:05 +0100

    feat: implement transaction detail page and technical info toggle

---
+++ native-app/src/screens/TransactionDetailScreen.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+export default function TransactionDetailScreen() {
+  return (
+    <View><Text>Transaction Details</Text></View>
+  );
+}
